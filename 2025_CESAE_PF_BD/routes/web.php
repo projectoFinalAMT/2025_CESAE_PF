81d0ff2a10338fbab5f57f06f15bd159fb6567bf
MarceloValente1990 <valente.marcelo1990@gmail.com>
2025-07-21 19:46:06 +0100

    Test

--- 2025_CESAE_PF_BD/routes/web.php
+++ 2025_CESAE_PF_BD/routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\homeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+Route::get('/home', function () {
     return view('welcome');
 });
+
+Route::get('/home', [homeController::class, 'index'])->name('casa');
